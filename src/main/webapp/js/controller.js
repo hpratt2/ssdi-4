cc5b6e60bc60ed18278cb1d6627a1baf2280c3fc
Jacob Greene <35856498+greenej10@users.noreply.github.com>
2018-04-10 17:14:25 -0400

    filter rating

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -1,5 +1,7 @@
 var app = angular.module('index',[]);
 app.controller('indexCtrl',function($scope,$http){
+	$scope.minRating = 0
+	$scope.maxRating = 100;
 	$scope.sortOptions = [
 		{name:'Avg. Rating', value:'-avgRating'},
 		{name:'Name: A - Z', value:'name'},
@@ -7,6 +9,25 @@ app.controller('indexCtrl',function($scope,$http){
 		{name:'Price: Low to High', value:'priceLow'},
 		{name:'Price: High to Low', value:'-priceHigh'}
 	];
+	
+	app.filter("ratingFilter", function(){
+		return function(wines, minRating, maxRating){
+		var wineLow = minRating;
+		var wineHigh = maxRating;
+		var filteredWines = [];
+		
+		for (var i=0; i<wines.length; i++){
+			var wLow = new Wine(wines[i].minRating),
+				wHigh = new Wine(wines[i].maxRating);
+			if(wLow>wineLow && wHigh<maxRating){
+				filteredWines.push(wines[i])
+			}
+		}
+		
+		return filteredWines;
+		};	
+	
+	
 	$http.get('rest/winelist').then(function(response){
 		$scope.status = response.status;
 		$scope.wines = response.data;
