bdcd4a173ee88c99eedaf2aa39722961d18268d4
Jacob Greene <35856498+greenej10@users.noreply.github.com>
2018-04-13 12:30:25 -0400

    New Test  Controller

--- src/main/java/winevault/api/IndexControllerTest.java
+++ src/main/java/winevault/api/IndexControllerTest.java
@@ -1,6 +1,13 @@
 package winevault.api;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
@@ -10,7 +17,19 @@ import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
 import org.junit.Test;
 
+import winevault.model.IWine;
+import winevault.model.Wine;
+import winevault.service.WineListService;
+
 public class IndexControllerTest extends JerseyTest {
+	
+	private List<IWine> test = new ArrayList<IWine>();
+	Wine w1 = new Wine(1, "White Blend", "Italy", 87.0, 10.0, 12.0);
+	Wine w2 = new Wine(2, "Portuguese Red", "Portugal", 87.0, 15.0, 15.0);
+	Wine w3 = new Wine(3, "Pinot Gris", "US", 87.33, 14.0, 27.0);
+	
+	WineListService wls = mock(WineListService.class);
+	IndexController ctrl = mock(IndexController.class);
 	
 	@Override
 	public Application configure() {
@@ -23,5 +42,12 @@ public class IndexControllerTest extends JerseyTest {
 	public void testGetWineList() {
 		Response r = target("/winelist").request().get();
 		assertEquals(200, r.getStatus());
+		
+		test.add(w1);
+		test.add(w2);
+		test.add(w3);
+		when(wls.getWineList()).thenReturn(test);
+		ctrl.getWineList();
+		verify(ctrl, times(1)).getWineList();
 	}
 }
